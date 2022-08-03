f88495a71c9f892b42d006a169ecf2fc3493192d
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-08-03 10:21:38 -0300

    init the repository

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const port = 3000;
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
